95c7aa6ace5dd014599fbe390fabdc5151899a16
lcoombe <lauren.e.coombe@gmail.com>
2026-08-05 07:21:34 -0700

    Remove debug statement

--- bin/ntjoin_overlap.py
+++ bin/ntjoin_overlap.py
@@ -98,9 +98,6 @@ def merge_overlapping(list_mxs, list_mx_info, source, target, nodes):
     target_trim_amount = target_cut
     if source_trim_amount > nodes[target].get_aligned_length() or \
             target_trim_amount > nodes[source].get_aligned_length():
-        print(f"NOTE: Skipping overlap trim between {nodes[source].contig} and "
-              f"{nodes[target].contig} - inferred overlap length exceeds a sequence length "
-              f"(source_trim={source_trim_amount}, target_trim={target_trim_amount})")
         return False
 
     nodes[source].end_adjust = source_cut
